0b3be09d45db4c1821bc6a5eae59d9fcd588a8b1
Abere Fejiro <aberefejiro@gmail.com>
2024-03-09 14:14:04 +0100

    :brush: done styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
         "inter-font": ["Inter", "sans-serif"],
         "lexend-font": ["Lexend Deca", "serif"],
       },
+      fontSize: {
+        h1: "clamp(1.77rem, 2.5vw, 2rem)",
+        base: "clamp(1rem, 1vw, 1.5rem)",
+        "2xl": "clamp(1.5rem, 1vw, 2rem)",
+        "sm": "clamp(0.875rem, 1vw, 1rem)",
+      },
+      screens: {
+        md: "900px",
+      },
     },
   },
   plugins: [],
